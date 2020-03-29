4b1c862aeeb003cd8a895de09b888bcffb503f5f
Lucas Foo <lucasfoo33@gmail.com>
2020-03-29 14:44:58 +0800

    initial commit

---
+++ background.js
@@ -0,0 +1,41 @@
+var download_url = [];
+var chunk_url;
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    download_url = []
+  }
+})
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (details.url.slice(-5) ==  ".m3u8") {
+      if(details.url.includes("index") || details.url.includes("playlist")){
+        download_url.push(details.url);
+      }
+    }
+  },
+  { urls: ["*://citcastmedia.nus.edu.sg/*", "https://s-cloudfront.cdn.ap.panopto.com/*"] }
+);
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            urlContains: "https://luminus.nus.edu.sg/modules/",
+            urlContains: "media/view"
+          }
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            urlContains: "https://luminus.nus.edu.sg/modules/",
+            urlContains: "web-lectures"
+          }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
---
+++ popup.js
@@ -0,0 +1,38 @@
+let detected = document.getElementById("video_detected");
+
+
+
+chrome.runtime.getBackgroundPage(function(bg) {
+  console.log(bg.download_url)
+  if (bg.download_url.length >= 1) {
+    if (bg.download_url.length >= 1){
+      var warningText = document.createElement("p")
+      warningText.innerHTML = "Warning! There may be a file without audio!";
+      warningText.style.color = "red";
+      document.body.appendChild(warningText);
+    }
+    detected.innerHTML = "Video Detected";
+    bg.download_url.forEach(url => {
+      command = ".\\ffmpeg.exe " +  "-i " + url + " -c copy -bsf:a aac_adtstoasc output.mp4";
+      var commandText = document.createElement("p")
+      commandText.innerHTML = command;
+      document.body.appendChild(commandText);
+      var copyButton = document.createElement("button");
+      copyButton.innerHTML = "Copy to clipboard";
+      copyButton.onclick = function(element) {
+        copyTextToClipboard(command);
+      };
+      document.body.appendChild(copyButton);
+    });
+  }
+});
+
+function copyTextToClipboard(text) {
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  copyFrom.blur();
+  document.body.removeChild(copyFrom);
+}
